f3837cdfdb43e2018be7eb8eac3301fad2bdec50
ryotah8755 <168844344+ryotah8755@users.noreply.github.com>
2024-10-01 16:09:49 +0000

    partner work 10/

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -3,6 +3,13 @@ package com.example.project;
 public class Student {
     // instance variables for 
     // first name (String)
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double sumTestScore;
+    private double testScoreCount;
+    private double highestTestScore;
+    private double averageTestScore;
     // last name (String)
     // graduation year (int)
     // sum of test scores (double).. should initialize at 0.0
@@ -11,61 +18,68 @@ public class Student {
  
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
-        //implement code here!
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
+        sumTestScore = 0.0;
+        testScoreCount = 0.0;
+        highestTestScore = 0.0;
+        averageTestScore = 0.0;
 
     }
  
     // returns firstName
     public String getFirstName() {
-        //implement code here!
-        return "";
+        return firstName;
     }
  
     // returns lastName
     public String getLastName() {
-        //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
-        //implement code here!
-        return 0.0;
+        return highestTestScore;
     }
 
-    public int getTestScoreCount(){
-        //implement code here!
-        return 0;
+    public double getTestScoreCount(){
+        return testScoreCount;
     }
 
     public int getGradYear(){
-        //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
-    public void setGradYear(int newGradYear) {
-        //implement code here!
+    public void setGradYear(int setGradYear) {
+        gradYear = setGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
     // and increments testScoreCount by 1
     // set new highest test score 
     public void addTestScore(double newTestScore) {
-        //implement code here!
+        testScoreCount = testScoreCount + 1.0 ;
+        sumTestScore = sumTestScore + newTestScore;
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
-        //implement code here!
-        return false;
+        if(averageTestScore >= 65.0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
-        //implement code here!
-        return 0.0;
+        averageTestScore = (double) sumTestScore/testScoreCount;
+        return averageTestScore;
     }
  
     // this method prints all info of a Student object to the console 
--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,10 @@
 public class StudentRunner{
-    public static void main(String[] args) {
-        //use this StudentRunner class if you need to test your code 
+    public static void main(String[] args) 
+    {
+        Student s = new Student("John", "Doe", 2025);
+        s.addTestScore(91.5);
+        s.addTestScore(95.6);
+        
+        
     }
 }
